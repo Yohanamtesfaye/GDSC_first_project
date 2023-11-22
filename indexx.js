42687ec5a67566cf41e623dfc1f68d6f49827fb2
Yohana <130049346+Yohanamtesfaye@users.noreply.github.com>
2023-11-22 15:25:15 +0300

    commit

---
+++ indexx.js
@@ -0,0 +1,12 @@
+//declare, initialization of variables using let
+let x=1,y=2,add,substract,divide,multiply;
+//arthimetic operations
+add = x+y;
+substract = x-y;
+divide = x/y;
+multiply = x*y;
+//print the operation
+console.log(add);
+console.log(substract);
+console.log(divide);
+console.log(multiply);
